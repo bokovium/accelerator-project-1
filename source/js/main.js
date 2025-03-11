8af49bb03c89e02ebd00a520513ec9f4ae3ab552
bokovium <bokyuri95@gmail.com>
2025-03-12 00:24:20 +0300

    test pp about mobile + video js

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,24 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+const videoButton = document.querySelector('.about__video-button');
+
+function loadVideo() {
+
+  const videoContainer = document.querySelector('.about__video');
+  videoContainer.querySelector('.about__video-image').remove();
+
+  const iframe = document.createElement('iframe');
+  const styleSheet = document.styleSheets[0];
+  iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1';
+  iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+  iframe.allowFullscreen = true;
+  iframe.width = '320px';
+  iframe.height = '170px';
+  iframe.style.border = 'none';
+
+  videoContainer.appendChild(iframe);
+
+  videoButton.style.display = 'none';
+
+  styleSheet.insertRule('.about__video::before {background-color: transparent;}', styleSheet.cssRules.length);
+}
+
+videoButton.addEventListener('click', loadVideo);
